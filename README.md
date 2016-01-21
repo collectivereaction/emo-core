35ea40cda928bac24794a0305ec233a93872b15f
Raid <ralbaya1@asu.edu>
2016-01-20 18:28:16 -0700

    Figured out socket

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"mvn clean install" 
+mvn clean install
+mvn package // Compile package
+java -cp target\uploader-1.0-SNAPSHOT.jar capstone.App // Run app
--- src/main/java/capstone/App.java
+++ src/main/java/capstone/App.java
@@ -1,5 +1,8 @@
 package capstone;
 
+import java.io.*;
+import java.net.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+ 
+        String hostName = "localhost";
+        int portNumber = 7474;
+ 
+		try {
+            Socket echoSocket = new Socket(hostName, portNumber);
+            BufferedReader in =
+                new BufferedReader(
+                    new InputStreamReader(echoSocket.getInputStream()));
+            while (true) {
+                System.out.println(in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
     }
 }
