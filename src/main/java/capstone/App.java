20aecc1dc88435e4a89457f3233e210bdb0681a9
Raid <ralbaya1@asu.edu>
2016-02-03 19:09:38 -0700

    Listening to game socket

--- src/main/java/capstone/App.java
+++ src/main/java/capstone/App.java
@@ -12,7 +12,7 @@ import com.amazonaws.services.dynamodbv2.document.Table;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -21,31 +21,50 @@ public class App
         DynamoDB dynamoDB = new DynamoDB(client);
 
         Table table = dynamoDB.getTable("pad-test");
- 
+		Table gameTable = dynamoDB.getTable("game-test");
+		
         String hostName = "localhost";
         int portNumber = 7474;
- 
+		int gamePortNumber = 7777;
+		
 		try {
+			//PAD socket
             Socket echoSocket = new Socket(hostName, portNumber);
             BufferedReader in =
-                new BufferedReader(
-                    new InputStreamReader(echoSocket.getInputStream()));
+                new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			
+			//Game socket
+			Socket gameEchoSocket = new Socket(hostName, gamePortNumber);
+            BufferedReader gameIn =
+                new BufferedReader(new InputStreamReader(gameEchoSocket.getInputStream()));
             while (true) {
                 String read = in.readLine();
 				String[] temp = read.split(",");
-				
+
 				String P = temp[1].trim();
 				String A = temp[2].trim();
 				
+				String gameRead = gameIn.readLine();
+				
+				String[] gameTemp = gameRead.split(",");
+
+				String gameTimeStamp = temp[0].trim();
+				String gameEvent = temp[1].trim();
+				
 				try {
 					table.putItem(new Item()
-					.withPrimaryKey("session", "test", "timestamp", (new Date()).toString())
+					.withPrimaryKey("session", "jake-pruitt", "timestamp", System.currentTimeMillis())
 					.withString("P", P)
 					.withString("A", A));
 					System.out.println("PutItem succeeded: " + read);
+					
+					gameTable.putItem(new Item()
+					.withPrimaryKey("session", "jake-pruitt", "timestamp", gameTimeStamp)
+					.withString("gameCode", gameEvent));
+					System.out.println("PutItem succeeded: " + gameRead);
 
 				} catch (Exception e) {
-					System.err.println("Unable to add movie: " + read);
+					System.err.println("Unable to add: " + read);
 					System.err.println(e.getMessage());
 					break;
 				}
@@ -57,6 +76,6 @@ public class App
             System.err.println("Couldn't get I/O for the connection to " +
                 hostName);
             System.exit(1);
-        } 
+        }
     }
 }
