9bc107b7b7c1172a32c98f6310c1af8cb86cd7c8
jakepruitt <jake@mapbox.com>
2016-02-10 22:02:31 -0700

    Add ServerThread class initial draft

---
+++ src/main/java/capstone/ServerThread.java
@@ -0,0 +1,51 @@
+package capstone;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class ServerThread implements Runnable {
+    public void run() {
+		AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("pad-test");
+		Table gameTable = dynamoDB.getTable("game-test");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(7777);
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+				String[] gameTemp = inputLine.split(",");
+
+				String gameTimeStamp = gameTemp[0].trim();
+				String gameEvent = gameTemp[1].trim();
+
+                try {
+					gameTable.putItem(new Item()
+					.withPrimaryKey("session", "jake-pruitt", "timestamp", gameTimeStamp)
+					.withString("gameCode", gameEvent));
+					System.out.println("PutItem succeeded: " + inputLine);
+                } catch (Exception e) {
+					System.err.println("Unable to add: " + inputLine);
+					System.err.println(e.getMessage());
+                    break;
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection");
+            System.exit(1);
+        }
+    }
+}
